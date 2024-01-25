c73830c696cc80112b288fba0ab80db48cec6ee5
gilahorn <gilahorn21@gmail.com>
2024-01-25 08:46:42 -0800

    photo

--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -26,35 +26,47 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address” )
-
+String url = "https://www.freepnglogos.com/uploads/cat-png/cute-cat-images-download-7.png";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component Image = createImage(url);
 		// 3. use the "createImage()" method below to initialize your Component
 
 		// 4. add the image to the quiz window
-
+quizWindow.add(Image);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String input = JOptionPane.showInputDialog("is this image too big");
 		// 7. print "CORRECT" if the user gave the right answer
+if (input.equals("yes")) {
+	JOptionPane.showMessageDialog(null, "correct");
+}
+else {
+	JOptionPane.showMessageDialog(null, "incorrect");
+}
 
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(Image);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+url = "https://pngimg.com/uploads/cat/cat_PNG50527.png";
+Image = createImage(url);
 		// 11. add the second image to the quiz window
-
+quizWindow.add(Image);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+ input = JOptionPane.showInputDialog("is this image too big");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+if( input.equals("no")) {
+	JOptionPane.showMessageDialog(null, "correct");
+}
+else {
+	JOptionPane.showMessageDialog(null, "incorrect");
+}
 	}
 
 	private Component createImage(String imageUrl) {
